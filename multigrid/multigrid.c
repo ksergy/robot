6bbb64c07af77b1ae024d65aee7f6793b492eff4
Sergy Kanaev <ksergy.91@gmail.com>
2016-06-15 21:46:30 +0300

    Continue multigrid implementation

--- multigrid/multigrid.c
+++ multigrid/multigrid.c
@@ -19,25 +19,38 @@ typedef struct pic_val {
 /************************** functions declarations **************************/
 static
 void grid_init(multigrid_t *host,
-               grid_t *g, grid_id_t id, pic_val_t v,
+               grid_t *g, grid_id_t id, grid_level_t level,
                grid_t *parent, const division_scheme_t *pos,
-               const picture_dimensions_t pic[PP_MAX], grid_level_t level);
+               const picture_dimensions_t pic[PP_MAX], pic_val_t v);
+
+static
+void grid_grid(grid_t *g);
+
+static
+void grid_setup_neighborhood_children_relations(grid_t *g,
+                                                bool recursive);
+
+static
+grid_id_t division_scheme_mul(const division_scheme_t *ds);
+
+static
+grid_t *multigrid_add_grid(multigrid_t *host, grid_id_t id);
 
 static
 pic_val_t picture_value(const picture_t *pic,
                         const picture_dimensions_t pp[PP_MAX]);
 
 static
-grid_id_t division_scheme_mul(const division_scheme_t *ds);
-
-static
-void grid_grid(multigrid_t *host, grid_t *g);
+void picture_divide(const picture_dimensions_t pic[PP_MAX],
+                    const division_scheme_t *ds,
+                    const division_scheme_t *pos,
+                    picture_dimensions_t ret[PP_MAX]);
 
 /************************** functions definitions **************************/
 void grid_init(multigrid_t *host,
-               grid_t *g, grid_id_t id, pic_val_t v,
+               grid_t *g, grid_id_t id, grid_level_t level,
                grid_t *parent, const division_scheme_t *pos,
-               const picture_dimensions_t pic[PP_MAX], grid_level_t level) {
+               const picture_dimensions_t pic[PP_MAX], pic_val_t v) {
     g->host = host;
     g->id = id;
     g->v = v.v;
@@ -101,11 +114,47 @@ pic_val_t picture_value(const picture_t *pic,
     return pv;
 }
 
-void grid_grid(multigrid_t *host, grid_t *g) {
+void grid_grid(grid_t *g) {
+    grid_id_t id_segment = g->id + 1;
+    grid_level_t l = g->level + 1;
+    multigrid_t *host = g->host;
+    grid_id_t lcap = *(grid_id_t *)vector_get(&host->level_capacity, l);
+    picture_dimensions_t pic[PP_MAX] = {
+        [PP_FROM] = g->from,
+        [PP_SIZE] = g->size
+    };
+
+    grid_id_t id_offset;
+    picture_dimensions_t child_pic[PP_MAX];
+    division_scheme_t pos;
+    grid_t *child;
+    pic_val_t child_pv;
+
     if (!g->should_grid)
         return;
-#error "Not implemented"
-    /* TODO */
+
+    g->grided = true;
+    for (id_offset = 0, pos.v[DS_X_AXIS] = 0;
+         pos.v[DS_X_AXIS] < host->ds.v[DS_X_AXIS];
+         ++pos.v[DS_X_AXIS]) {
+        for (pos.v[DS_Y_AXIS] = 0; pos.v[DS_Y_AXIS] < host->ds.v[DS_Y_AXIS];
+             id_offset += lcap, ++pos.v[DS_Y_AXIS]) {
+            picture_divide(pic, &host->ds, &pos, child_pic);
+            child_pv = picture_value(&host->pic, child_pic);
+
+            grid_id_t id = id_segment + id_offset;
+
+            child = multigrid_add_grid(host, id);
+            grid_init(host, child, id, l, g, &pos, child_pic, child_pv);
+
+            grid_grid(child);
+        }
+    }
+
+    /* TODO make children relations setup selectable with a flag
+     * for ceratain situations
+     */
+    grid_setup_neighborhood_children_relations(g, true);
 }
 
 /************************** API **************************/
@@ -161,7 +210,7 @@ void multigrid_init(multigrid_t *mg,
 
     pic_value = picture_value(&pic, pp);
 
-    grid_init(mg, grid_0, 0, pic_value, NULL, &pos_0, pp, 0);
+    grid_init(mg, grid_0, 0, 0, NULL, &pos_0, pp, pic_value);
 
     mg->id_0 = grid_0;
 }
@@ -177,7 +226,7 @@ void multigrid_purge(multigrid_t *mg) {
 void multigrid_grid(multigrid_t *mg) {
     assert(mg);
 
-    grid_grid(mg, mg->id_0);
+    grid_grid(mg->id_0);
 }
 
 list_t grid_id_to_path(multigrid_t *mg, grid_id_t id) {
