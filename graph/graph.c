65bd5e4a3265a1b85be603ad101fcbb683969131
Sergy Kanaev <ksergy.91@gmail.com>
2016-07-27 23:12:40 +0300

    Reject from multi-graph in graph library implementation and switch to single graph with user data pointer provided.
        TODO: Fix tests for graph library due to API changes

--- graph/graph.c
+++ graph/graph.c
@@ -2,14 +2,16 @@
 
 #include <assert.h>
 
-void graph_init(graph_t *g, graph_vertex_idx_t vertices_number, bool directed) {
+void graph_init(graph_t *g,
+                graph_vertex_idx_t vertices_number,
+                bool directed) {
     assert(g);
 
     g->vertices_number = vertices_number;
     g->edges_number = 0;
     g->directed = directed;
 
-    avl_tree_init(&g->adjacency_map, true, sizeof(graph_edge_count_t));
+    avl_tree_init(&g->adjacency_map, false, 0);
 }
 
 void graph_deinit(graph_t *g) {
@@ -20,7 +22,8 @@ void graph_deinit(graph_t *g) {
 
 graph_edge_idx_t graph_add_edge(graph_t *g,
                                 graph_vertex_idx_t from,
-                                graph_vertex_idx_t to) {
+                                graph_vertex_idx_t to,
+                                void *node_data) {
     avl_tree_node_t *atn;
     bool inserted = false;
     graph_edge_idx_t e;
@@ -33,39 +36,32 @@ graph_edge_idx_t graph_add_edge(graph_t *g,
             e = graph_edge_idx(g->vertices_number, to, from);
             atn = avl_tree_add_or_get(&g->adjacency_map, e, &inserted);
 
-            switch (inserted) {
-                case true:
-                    *(graph_edge_count_t *)(atn->data) = 0;
+            atn->data = node_data;
 
-                default:
-                    (*(graph_edge_count_t *)(atn->data)) ++;
-            }
-
-            ++ g->edges_number;
+            g->edges_number += inserted;
 
         default:
+            inserted = false;
+
             e = graph_edge_idx(g->vertices_number, from, to);
             atn = avl_tree_add_or_get(&g->adjacency_map, e, &inserted);
 
-            switch (inserted) {
-                case true:
-                    *(graph_edge_count_t *)(atn->data) = 0;
+            atn->data = node_data;
 
-                default:
-                    (*(graph_edge_count_t *)(atn->data)) ++;
-            }
-
-            ++ g->edges_number;
+            g->edges_number += inserted;
     }
 
     return e;
 }
 
-graph_edge_count_t graph_remove_edge(graph_t *g,
+graph_edge_found_t graph_remove_edge(graph_t *g,
                                      graph_vertex_idx_t from, graph_vertex_idx_t to) {
+    graph_edge_found_t gef = {
+        .found = false,
+        .data = NULL
+    };
     avl_tree_node_t *atn;
     graph_edge_idx_t e;
-    graph_edge_count_t c = 0;
 
     assert(g);
     assert(g->vertices_number > from && g->vertices_number > to);
@@ -74,60 +70,63 @@ graph_edge_count_t graph_remove_edge(graph_t *g,
         case false:
             e = graph_edge_idx(g->vertices_number, to, from);
             atn = avl_tree_get(&g->adjacency_map, e);
-            if (atn && *(graph_edge_count_t *)(atn->data)) {
-                c = *(graph_edge_count_t *)(atn->data) - 1;
-                if (! (-- *(graph_edge_count_t *)(atn->data)))
-                    avl_tree_remove(&g->adjacency_map, e);
+            if (atn) {
+                gef.found = true;
+                gef.data = atn->data;
+                avl_tree_remove(&g->adjacency_map, e);
 
                 -- g->edges_number;
             }
         default:
             e = graph_edge_idx(g->vertices_number, from, to);
             atn = avl_tree_get(&g->adjacency_map, e);
-            if (atn && *(graph_edge_count_t *)(atn->data)) {
-                c = *(graph_edge_count_t *)(atn->data) - 1;
-                if (! (-- *(graph_edge_count_t *)(atn->data)))
-                    avl_tree_remove(&g->adjacency_map, e);
+            if (atn) {
+                gef.found = true;
+                gef.data = atn->data;
+                avl_tree_remove(&g->adjacency_map, e);
 
                 -- g->edges_number;
             }
     }
 
-    return c;
+    return gef;
 }
 
-graph_edge_count_t graph_remove_edge_idx(graph_t *g, graph_edge_idx_t idx) {
+graph_edge_found_t graph_remove_edge_idx(graph_t *g, graph_edge_idx_t idx) {
+    graph_edge_found_t gef = {
+        .found = false,
+        .data = NULL
+    };
     avl_tree_node_t *atn;
-    graph_edge_count_t c = 0;
 
     assert(g);
 
     switch (g->directed) {
         case false:
             atn = avl_tree_get(&g->adjacency_map, idx);
-            if (atn && *(graph_edge_count_t *)(atn->data)) {
-                c = *(graph_edge_count_t *)(atn->data) - 1;
-                if (! (-- *(graph_edge_count_t *)(atn->data)))
-                    avl_tree_remove(&g->adjacency_map, idx);
+            if (atn) {
+                gef.found = true;
+                gef.data = atn->data;
+                avl_tree_remove(&g->adjacency_map, idx);
 
                 -- g->edges_number;
             }
         default:
             idx = graph_edge_inverse_idx(g->vertices_number, idx);
             atn = avl_tree_get(&g->adjacency_map, idx);
-            if (atn && *(graph_edge_count_t *)(atn->data)) {
-                c = *(graph_edge_count_t *)(atn->data) - 1;
-                if (! (-- *(graph_edge_count_t *)(atn->data)))
-                    avl_tree_remove(&g->adjacency_map, idx);
+            if (atn) {
+                gef.found = true;
+                gef.data = atn->data;
+                avl_tree_remove(&g->adjacency_map, idx);
 
                 -- g->edges_number;
             }
     }
 
-    return c;
+    return gef;
 }
 
-graph_edge_count_t graph_test_edge(graph_t *g,
+graph_edge_found_t graph_test_edge(graph_t *g,
                                    graph_vertex_idx_t from, graph_vertex_idx_t to) {
     avl_tree_node_t *atn;
 
@@ -139,10 +138,12 @@ graph_edge_count_t graph_test_edge(graph_t *g,
         graph_edge_idx(g->vertices_number, from, to)
     );
 
-    return atn ? *(graph_edge_count_t *)(atn->data) : 0;
+    return atn
+            ? (graph_edge_found_t){ .found = true, .data = atn->data}
+            : (graph_edge_found_t){ .found = false, .data = NULL};
 }
 
-graph_edge_count_t graph_test_edge_idx(graph_t *g, graph_edge_idx_t idx) {
+graph_edge_found_t graph_test_edge_idx(graph_t *g, graph_edge_idx_t idx) {
     avl_tree_node_t *atn;
 
     assert(g);
@@ -152,5 +153,7 @@ graph_edge_count_t graph_test_edge_idx(graph_t *g, graph_edge_idx_t idx) {
         idx
     );
 
-    return atn ? *(graph_edge_count_t *)(atn->data) : 0;
+    return atn
+            ? (graph_edge_found_t){ .found = true, .data = atn->data}
+            : (graph_edge_found_t){ .found = false, .data = NULL};
 }
--- graph/graph.h
+++ graph/graph.h
@@ -11,33 +11,44 @@
 struct graph;
 typedef struct graph graph_t;
 
+struct graph_edge_found;
+typedef struct graph_edge_found graph_edge_found_t;
+
 typedef uint32_t graph_vertex_idx_t;
-typedef uint64_t graph_edge_count_t;
 typedef uint64_t graph_edge_idx_t;
+typedef uint64_t graph_edge_count_t;
 
 struct graph {
     graph_vertex_idx_t vertices_number;
     graph_edge_count_t edges_number;
     bool directed;
 
-    /* key = graph_edge_idx(from, to) */
+    /* key = graph_edge_idx(from, to), data = node data ptr */
     avl_tree_t adjacency_map;
 };
 
-void graph_init(graph_t *g, graph_vertex_idx_t vertices_number, bool directed);
+struct graph_edge_found {
+    bool found;
+    void *data;
+};
+
+void graph_init(graph_t *g,
+                graph_vertex_idx_t vertices_number,
+                bool directed);
 void graph_deinit(graph_t *g);
 
 graph_edge_idx_t graph_add_edge(graph_t *g,
-                                graph_vertex_idx_t from, graph_vertex_idx_t to);
-graph_edge_count_t graph_remove_edge(graph_t *g,
+                                graph_vertex_idx_t from, graph_vertex_idx_t to,
+                                void *node_data);
+graph_edge_found_t graph_remove_edge(graph_t *g,
                                      graph_vertex_idx_t from,
                                      graph_vertex_idx_t to);
-graph_edge_count_t graph_remove_edge_idx(graph_t *g,
+graph_edge_found_t graph_remove_edge_idx(graph_t *g,
                                          graph_edge_idx_t idx);
-graph_edge_count_t graph_test_edge(graph_t *g,
+graph_edge_found_t graph_test_edge(graph_t *g,
                                    graph_vertex_idx_t from,
                                    graph_vertex_idx_t to);
-graph_edge_count_t graph_test_edge_idx(graph_t *g,
+graph_edge_found_t graph_test_edge_idx(graph_t *g,
                                        graph_edge_idx_t idx);
 
 static inline
